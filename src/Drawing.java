3d535a053fb2d72155828e20371d55e77f8f172b
Grisha <greg.pelipenko@gmail.com>
2022-07-20 23:37:34 +0300

    spiral

--- src/Drawing.java
+++ src/Drawing.java
@@ -12,19 +12,24 @@ public class Drawing extends Canvas {
     }
 
     public void paint(Graphics g) {
+        int a=30;
+        int r=0;
+        for(; r<1000;) {
 
-        for (int x = 0; x < 1000; x = x + 200) {
-            for (int y = 0; y < 1000; y = y + 200) {
-
-                g.fillRect(x, y, 100, 100);
-            }
+            g.drawArc(500-r, 500-r, r+r, r+r, a, 40);
+            r=r+1;
+            a=a+30;
         }
-        for (int x = 100; x < 1000; x = x + 200) {
-            for (int y = 100; y < 1000; y = y + 200) {
-
-                g.fillRect(x, y, 100, 100);
-            }
-        }
+//        g.drawArc(500,500,101,101,30,30);
+//        g.drawArc(500,500,102,102,60,30);
+//        g.drawArc(500,500,103,103,90,30);
+//        g.drawArc(500,500,104,104,120,30);
+//        g.drawArc(500,500,105,105,150,30);
+//        g.drawArc(500,500,106,106,180,30);
+//        g.drawArc(500,500,107,107,210,30);
+//        g.drawArc(500,500,108,108,240,30);
+//        g.drawArc(500,500,109,109,270,30);
+//        g.drawArc(500,500,110,110,300,30);
+//        g.drawArc(500,500,111,111,330,30);
     }
-
 }
