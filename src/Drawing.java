10ae45b18722e9edc845f9f8ce04ec3cb4051988
grisha <greg.pelipenko@mail.com>
2022-09-10 23:21:25 +0300

    abstrakcija

--- src/Drawing.java
+++ src/Drawing.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+import java.util.Random;
 import javax.swing.JFrame;
 
 public class Drawing extends Canvas {
@@ -12,24 +13,51 @@ public class Drawing extends Canvas {
     }
 
     public void paint(Graphics g) {
-        int a=30;
-        int r=0;
-        for(; r<1000;) {
+        Random r = new Random();
 
-            g.drawArc(500-r, 500-r, r+r, r+r, a, 40);
-            r=r+1;
-            a=a+30;
+
+        int x1;
+        int y1;
+
+        int x2 = 500;
+        int y2 = 400;
+
+        int red = 127;
+        int green = 127;
+        int blue = 127;
+
+
+        for (int a = 0;; a = a + 1) {
+
+
+            int x = r.nextInt(3);
+            x = x - 1;
+            int y = r.nextInt(3);
+            y = y - 1;
+
+            red = red + r.nextInt(3) - 1;
+            green = green + r.nextInt(3) - 1;
+            blue = blue + r.nextInt(3) - 1;
+
+            if (red < 0) red = 0;
+            if (green < 0) green = 0;
+            if (blue < 0) blue = 0;
+            if (red > 255) red = 255;
+            if (green > 255) green = 255;
+            if (blue > 255) blue = 255;
+
+            g.setColor(new Color(red, green, blue));
+
+
+            x1 = x2;
+            y1 = y2;
+            x2 = x2 + x;
+            y2 = y2 + y;
+            if (x2 < 10) x2 = 10;
+            if (y2 < 10) y2 = 10;
+            if (x2 >= 1000) x2 = 999;
+            if (y2 >= 1000) y2 = 999;
+            g.drawLine(x1, y1, x2, y2);
         }
-//        g.drawArc(500,500,101,101,30,30);
-//        g.drawArc(500,500,102,102,60,30);
-//        g.drawArc(500,500,103,103,90,30);
-//        g.drawArc(500,500,104,104,120,30);
-//        g.drawArc(500,500,105,105,150,30);
-//        g.drawArc(500,500,106,106,180,30);
-//        g.drawArc(500,500,107,107,210,30);
-//        g.drawArc(500,500,108,108,240,30);
-//        g.drawArc(500,500,109,109,270,30);
-//        g.drawArc(500,500,110,110,300,30);
-//        g.drawArc(500,500,111,111,330,30);
     }
 }
